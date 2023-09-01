a8f4c92b7b3cbdd5fafe2e29a6110c7ddc13038e
Mohammad Shamchi <shamchimohammad@gmail.com>
2023-09-01 16:30:30 +0300

    After adding Styles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,27 +22,27 @@ class DatabaseSeeder extends Seeder
 
         Listing::factory(6)->create();
 
-        // Listing::create([
-        //     'title' => 'Backend Developer',
-        //     'tags' => 'laravel, php, api',
-        //     'company' => 'Skynet Systems',
-        //     'location' => 'Newark, NJ',
-        //     'email' => 'email4@email.com',
-        //     'website' => 'https://www.skynet.com',
-        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo
-        //     reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat
-        //     consequatur! Expedita ab consectetur tenetur delensiti?'
-        // ]);
-        // Listing::create([
-        // 'title' => 'Junior Developer',
-        // 'tags' => 'laravel, php, javascript',
-        // 'company' => 'Wonka Industries',
-        // 'location' => 'Boston, MA',
-        // 'email' => 'email4@email.com',
-        // 'website' => 'https://www.wonka.com',
-        // 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit
-        // quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab
-        // consectetur tenetur delensiti?'
-        // ]);
+        Listing::create([
+            'title' => 'Backend Developer',
+            'tags' => 'laravel, php, api',
+            'company' => 'Skynet Systems',
+            'location' => 'Newark, NJ',
+            'email' => 'email4@email.com',
+            'website' => 'https://www.skynet.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo
+            reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat
+            consequatur! Expedita ab consectetur tenetur delensiti?'
+        ]);
+        Listing::create([
+        'title' => 'Junior Developer',
+        'tags' => 'laravel, php, javascript',
+        'company' => 'Wonka Industries',
+        'location' => 'Boston, MA',
+        'email' => 'email4@email.com',
+        'website' => 'https://www.wonka.com',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit
+        quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab
+        consectetur tenetur delensiti?'
+        ]);
     }
 }
